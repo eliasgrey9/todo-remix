158e18ed144ae68e774b550b5c780d3f6718b6ce
Elias Grey <eliasgrey@Isaiahs-MacBook-Pro.local>
2023-01-13 10:57:33 -0800

    Added components

---
+++ app/component/Add.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+export default function Add({ onAdd }) {
+  const [task, setTask] = useState("");
+  const [description, setDescription] = useState("");
+  const [time, setTime] = useState("");
+  const submit = (e) => {
+    e.preventDefault();
+    if (!task || !time) {
+      alert("You can't submit an empty task or time!");
+      return;
+    }
+    onAdd({ task, description, time });
+    setTask("");
+    setDescription("");
+    setTime("");
+  };
+  return (
+    <div>
+      <h2>Add Task</h2>
+      <form className="add-form">
+        <div className="form-control">
+          <label>Task</label>
+          <input
+            type="text"
+            value={task}
+            placeholder="Add Task"
+            onChange={(e) => setTask(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label>Description</label>
+          <input
+            type="text"
+            value={description}
+            placeholder="Description"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label>Set Day & Time</label>
+          <input
+            type="text"
+            value={time}
+            placeholder="Day & Time"
+            onChange={(e) => setTime(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-block" onClick={submit}>
+          Save task
+        </button>
+      </form>
+    </div>
+  );
+}
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,32 +1,66 @@
+import { useState } from "react";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import Add from "../component/Add";
+import styles from "../styles/task.css";
+export const links = () => {
+  return [{ rel: "stylesheet", href: styles }];
+};
 export default function Index() {
+  const [AddSection, setAddSection] = useState(false);
+  const [tasks, setTasks] = useState([
+    { id: 1, task: "First task", description: "Description 1", time: "1:00" },
+    { id: 2, task: "Second task", description: "Description 2", time: "2:00" },
+  ]);
+  // Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((tasks) => tasks.id !== id));
+  };
+  // Toggle add section
+  const toggleShow = () => {
+    setAddSection(!AddSection);
+  };
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="container">
+      <header className="header">
+        <h1>Todo Application</h1>
+        <button
+          className="btn"
+          style={
+            AddSection
+              ? { backgroundColor: "red" }
+              : { backgroundColor: "green" }
+          }
+          onClick={toggleShow}
+        >
+          {AddSection ? "Close" : "Add"}
+        </button>
+      </header>
+      {AddSection && <Add onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <div className="task-section">
+          {tasks.map((task) => (
+            <div className="task" key={task.id}>
+              <h3 className="task-title">{task.task}</h3>
+              <p className="task-description">{task.description}</p>
+              <p className="task-time">{task.time}</p>
+              <button
+                className="delete-icon"
+                onClick={() => deleteTask(task.id)}
+              >
+                <RiDeleteBin5Line />
+              </button>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <h1>No Task</h1>
+      )}
     </div>
   );
 }
